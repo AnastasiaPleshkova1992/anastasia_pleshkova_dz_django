00aa0e995ff03c697ca6014b0b9d0750d4da4d59
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-05-02 18:58:01 +0500

    add homework 21.1 Zadanie_1

--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-def home(request):
-    context = {'product_list': Product.objects.all()}
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Message from {name} (Tel: {phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
 
 
-def product(request, pk):
-    context = {'product': get_object_or_404(Product, pk=pk)}
-    return render(request, 'catalog/product.html', context)
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f'Message from {name} (Tel: {phone}): {message}')
+        return render(request, self.template_name)
+
