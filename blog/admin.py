7ac35b73277b61b3fbb6587d8ef500b9ee62debc
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-05-03 19:54:46 +0500

    add homework 21.1 add in admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from blog.models import BlogPost
 
 
 @admin.register(BlogPost)
-class ProductAdmin(admin.ModelAdmin):
+class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug",)
     list_filter = ("published",)
     search_fields = ("title",)
