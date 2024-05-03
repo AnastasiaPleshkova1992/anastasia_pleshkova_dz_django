c93931e8498105ca0b60f895838cd7433b50fecb
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-05-03 13:53:14 +0500

    add homework 21.1 Zadanie_2

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -10,12 +10,12 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read_catalog():
-        with open('catalog_data.json', encoding='UTF-8') as file:
+        with open("catalog_data.json", encoding="UTF-8") as file:
             return json.load(file)
 
     def handle(self, *args, **options):
         with connection.cursor() as cursor:
-            cursor.execute('TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE;')
+            cursor.execute("TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE;")
 
         Category.objects.all().delete()
         Product.objects.all().delete()
@@ -26,16 +26,22 @@ class Command(BaseCommand):
         for category in Command.json_read_catalog():
             if category["model"] == "catalog.category":
                 category_for_create.append(
-                    Category(category_name=category["fields"]["category_name"],
-                             category_description=category["fields"]["category_description"]))
+                    Category(
+                        category_name=category["fields"]["category_name"],
+                        category_description=category["fields"]["category_description"],
+                    )
+                )
 
         Category.objects.bulk_create(category_for_create)
 
         for product in Command.json_read_catalog():
             if product["model"] == "catalog.product":
                 product_for_create.append(
-                    Product(product_name=product["fields"]["product_name"],
-                            product_description=product["fields"]["product_description"],
-                            category=Category.objects.get(pk=product["fields"]["category"]),
-                            price=product["fields"]["price"]))
+                    Product(
+                        product_name=product["fields"]["product_name"],
+                        product_description=product["fields"]["product_description"],
+                        category=Category.objects.get(pk=product["fields"]["category"]),
+                        price=product["fields"]["price"],
+                    )
+                )
         Product.objects.bulk_create(product_for_create)
--- catalog/views.py
+++ catalog/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/product_list.html'
+    template_name = "catalog/product_list.html"
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'catalog/product_detail.html'
+    template_name = "catalog/product_detail.html"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:home")
 
 
 class ContactsView(TemplateView):
-    template_name = 'catalog/contacts.html'
+    template_name = "catalog/contacts.html"
 
     def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            name = request.POST.get('name')
-            phone = request.POST.get('phone')
-            message = request.POST.get('message')
-            print(f'Message from {name} (Tel: {phone}): {message}')
+        if request.method == "POST":
+            name = request.POST.get("name")
+            phone = request.POST.get("phone")
+            message = request.POST.get("message")
+            print(f"Message from {name} (Tel: {phone}): {message}")
         return render(request, self.template_name)
-
