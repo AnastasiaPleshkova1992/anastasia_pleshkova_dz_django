a6b8451325d3977f9bc600dc7830e7c57468c1bd
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-05-17 18:20:57 +0500

    add Zadanie_2,3 22.1

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,23 @@
 from django import forms
+from django.forms import BooleanField
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StileFormMixin:
+    """Class Mixin for stilization forms"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+
+
+class ProductForm(StileFormMixin, forms.ModelForm):
     """Form for adding a new product."""
     banned_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
@@ -26,3 +40,9 @@ class ProductForm(forms.ModelForm):
             if word in cleaned_data.lower():
                 raise forms.ValidationError(f'Нельзя использовать в описании слово "{word}"')
         return cleaned_data
+
+
+class VersionForm(StileFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = "__all__"
